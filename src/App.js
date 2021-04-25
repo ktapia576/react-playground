c436e948a22deb1d3dc4a98a557ea5c5e7a2246e
ktapia576 <ktapia576@hotmail.com>
2021-04-25 17:26:42 -0400

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Footer from "./Footer";
+
 function App() {
   return (
     <div className="App">
       <p> Hello World! </p>
+      <ul> 
+        <li>This is test </li>
+        <li>I like rock climbing</li>
+      </ul>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>This is a footer element that will be nested in App.js</p>
+        </footer>
+    )
+}
+
+export default Footer;

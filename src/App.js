ab6dc38d6de6fef977310951e44d2bd67f69b9ee
ktapia576 <ktapia576@hotmail.com>
2021-04-25 22:02:10 -0400

    Add Comment component with mock data and helper functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Welcome from "./Welcome";
 import HelloWorld from "./HelloWorld";
+import Comment from "./Comment";
 import Footer from "./Footer";
 
 function App() {
@@ -8,9 +9,21 @@ function App() {
       <Welcome name="Kevin"/>
       <Welcome name="Lukas"/>
       <HelloWorld />
+      <Comment date={comment.date} text={comment.text} author={comment.author} />
       <Footer />
     </div>
   );
 }
+
+// Test Data
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
 
 export default App;
---
+++ src/Comment.js
@@ -0,0 +1,18 @@
+import UserInfo from "./UserInfo";
+import formatDate from "./Utils/formatDate";
+
+function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+}
+
+export default Comment;

9ffcfc469be86040f861aa1059b6e726d223929a
ktapia576 <ktapia576@hotmail.com>
2021-05-12 23:12:00 -0400

    fix bug and format code

--- src/FlavorForm.js
+++ src/FlavorForm.js
@@ -6,7 +6,7 @@ class FlavorForm extends React.Component {
         this.state = {value: "lime"};
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
@@ -21,17 +21,17 @@ class FlavorForm extends React.Component {
     render() {
         return(
             <form onSubmit={this.handleSubmit}>
-            <label>
-            Pick your favorite flavor:
-            <select value={this.state.value} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-            </select>
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+                <label>
+                Pick your favorite flavor:
+                <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="grapefruit">Grapefruit</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 
